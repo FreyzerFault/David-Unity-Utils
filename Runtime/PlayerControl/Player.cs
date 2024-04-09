fee5d3a7e48bcc799ceba50c660db4a4a3bbb741
David Martinez Alcantara <martinezalcantara29@gmail.com>
2024-04-09 14:27:59 +0200

    Update Player.cs

--- Runtime/PlayerControl/Player.cs
+++ Runtime/PlayerControl/Player.cs
@@ -4,28 +4,31 @@ using UnityEngine.InputSystem;
 
 namespace DavidUtils.PlayerControl
 {
-    public class Player: MonoBehaviour
+    public class Player : MonoBehaviour
     {
         public enum PlayerState
         {
             Playing,
             Pause
         }
+
         protected PlayerState _state = PlayerState.Playing;
         protected Vector3 _moveInput = Vector3.zero;
-        
+
+        public event Action<Vector3> OnPlayerMove;
         public event Action<Vector3> OnPlayerStop;
-        
+
         protected Vector3 Position => transform.position;
         protected Vector3 Forward => transform.forward;
         protected Vector3 Right => transform.right;
-        
+
         public PlayerState State
         {
             get => _state;
             set
             {
-                if (value == _state) return;
+                if (value == _state)
+                    return;
                 HandleStateChanged(value);
                 _state = value;
             }
@@ -38,14 +41,17 @@ namespace DavidUtils.PlayerControl
         protected virtual void Update() { }
 
         protected virtual void HandleStateChanged(PlayerState newState) { }
-        
+
         protected virtual void OnMove(InputValue value)
         {
-            if (_state == PlayerState.Pause) return;
+            if (_state == PlayerState.Pause)
+                return;
             _moveInput = value.Get<Vector2>();
-            
+
             if (_moveInput == Vector3.zero)
                 OnPlayerStop?.Invoke(Position);
+            else
+                OnPlayerMove?.Invoke(_moveInput);
         }
     }
 }
