50b517c1316eb45afd2376592efb8248a3d0fb2c
FreyzerFault <martinezalcantara29@gmail.com>
2025-10-01 14:49:20 +0200

    Cambios en verano

---
+++ Runtime/AudioManager.cs
@@ -0,0 +1,187 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Audio;
+using Utils;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    public enum AudioType { Music, Sfx }
+    
+    public Transform musicParent;
+    public Transform sfxParent;
+    
+    public AudioMixerGroup musicMixerGroup;
+    public AudioMixerGroup sfxMixerGroup;
+    
+    private List<AudioSource> _musicSources;
+    private List<AudioSource> _sfxSources;
+    
+    private AudioSource _playingMusic;
+    private bool IsPlayingMusic => _playingMusic != null && _playingMusic.isPlaying;
+    
+    private List<AudioSource> SourceListByType(AudioType type) => type switch
+    {
+        AudioType.Music => _musicSources,
+        AudioType.Sfx => _sfxSources,
+        _ => null
+    };
+
+    protected override void Awake()
+    {
+        base.Awake();
+        
+        _musicSources = musicParent.GetComponentsInChildren<AudioSource>().ToList();
+        _sfxSources = sfxParent.GetComponentsInChildren<AudioSource>().ToList();
+        
+        musicMixerGroup ??= _musicSources.First()?.outputAudioMixerGroup;
+        sfxMixerGroup ??= _sfxSources.First()?.outputAudioMixerGroup;
+    }
+
+
+    #region SFX
+    
+    public AudioSource PlaySfx(AudioClip clip, float volume = 1f, float pitch = 1f) => Play(clip, AudioType.Sfx, volume, pitch);
+
+    public void PlaySfx(int index) => _sfxSources[index].Play();
+    public void PlaySfx(string clipName) => 
+        _sfxSources.First(source => source.clip.name == clipName).Play();
+
+    public void StopSfx(int index) => _sfxSources[index].Stop();
+    public void StopSfx(string clipName) => 
+        _sfxSources.First(source => source.clip.name == clipName).Stop();
+    
+    public void StopAllSfxs()
+    {
+        foreach (AudioSource sfxSource in _sfxSources) 
+            sfxSource.Stop();
+    }
+    
+    private AudioSource AddSfx(AudioClip clip) => AddAudio(clip, AudioType.Sfx);
+    private AudioSource InstantiateSfxSource() => InstantiateSource(AudioType.Sfx);
+
+    #endregion
+    
+
+
+    #region MUSIC
+    
+    public void  PlayMusic(AudioClip clip, float volume = 1f, float pitch = 1f)
+    {
+        if (IsPlayingMusic)
+            StopMusic();
+        
+        Play(clip, AudioType.Music, volume, pitch);
+    }
+
+    public void PlayMusic(int index) => PlayMusic(_musicSources[index]);
+    public void PlayMusic(string clipName) => 
+        PlayMusic(_musicSources.First(source => source.clip.name == clipName));
+    
+    private void PlayMusic(AudioSource musicSource)
+    {
+        if (IsPlayingMusic)
+            StopMusic();
+
+        _playingMusic = musicSource;
+        musicSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _playingMusic?.Stop();
+        _playingMusic = null;
+    }
+    
+    private AudioSource AddMusic(AudioClip clip) => AddAudio(clip, AudioType.Music);
+    private AudioSource InstantiateMusicSource() => InstantiateSource(AudioType.Music);
+    
+    #endregion
+
+
+    #region AUDIO CREATION
+    
+    private AudioSource AddAudio(AudioClip clip, AudioType type)
+    {
+        AudioSource source = type switch
+        {
+            AudioType.Music => InstantiateMusicSource(),
+            AudioType.Sfx => InstantiateSfxSource(),
+            _ => null
+        };
+        
+        if (source == null) return null;
+        
+        source.clip = clip;
+        
+        return source;
+    }
+    
+
+    private AudioSource InstantiateSource(AudioType type)
+    {
+        string objName = type switch
+        {
+            AudioType.Music => $"Music Source {_musicSources.Count}",
+            AudioType.Sfx => $"SFX Source {_sfxSources.Count}",
+            _ => ""
+        };
+        Transform parent = type switch
+        {
+            AudioType.Music => musicParent,
+            AudioType.Sfx => sfxParent,
+            _ => null
+        };
+        AudioMixerGroup mixerGroup = type switch
+        {
+            AudioType.Music => musicMixerGroup,
+            AudioType.Sfx => sfxMixerGroup,
+            _ => null
+        };
+        bool playOnAwake = false;
+        bool loop = type == AudioType.Music;
+        
+        // INSTANTIATE GameObject
+        GameObject sourceObj = new(objName);
+        sourceObj.transform.SetParent(parent);
+        
+        // ADD AudioSource COMPONENT
+        AudioSource source = sourceObj.AddComponent<AudioSource>();
+        source.outputAudioMixerGroup = mixerGroup;
+        source.playOnAwake = playOnAwake;
+        source.loop = loop;
+        
+        // ADD to AudioSource LIST
+        SourceListByType(type)?.Add(source);
+        
+        return source;
+    }
+
+    #endregion
+
+
+    #region PLAYING
+
+    private AudioSource Play(AudioClip clip, AudioType type = AudioType.Sfx, float volume = 1f, float pitch = 1f)
+    {
+        // Search for the Clip in Sources
+        AudioSource source = SourceListByType(type).FirstOrDefault(source => source.clip == clip);
+        
+        // If not found, search for an empty source to set the clip
+        if (source == null)
+        {
+            source = SourceListByType(type).FirstOrDefault(s => s.clip == null);
+            if (source != null) source.clip = clip;
+        }
+        
+        // If no empty source, create a new one
+        if (source == null) source = AddAudio(clip, type);
+
+        source.volume = volume;
+        source.pitch = pitch;
+        source.Play();
+        return source;
+    }
+
+    #endregion
+}
---
+++ Runtime/GameManager.cs
@@ -0,0 +1,175 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utils;
+
+/// <summary>
+///		Game Manager manage States, Scenes and global persistent data between ALL the Game
+///		SingletonPersistent: Singleton + DDOL (Don't Destroy On scene Load)
+///
+///		Change the State with GameManager.Instance.State = GameState
+///		Suscribe to onGameStateChanged in each component that depends on it
+/// </summary>
+public class GameManager : SingletonPersistent<GameManager>
+{
+	public enum GameState { MainMenu, GameOver, Playing, Paused }
+	
+	[SerializeField] private GameState initialState = GameState.MainMenu;
+	private GameState _state;
+	
+	public GameState State
+	{
+		get => _state;
+		set
+		{
+			_state = value;
+			OnStateChange(value);
+		}
+	}
+	
+	public bool IsPlaying => _state == GameState.Playing;
+	public bool IsPaused => _state == GameState.Paused;
+	
+	public Action onGameStateChanged; // Suscribe to this to update by the GameState
+	
+	
+	protected override void Awake()
+	{
+		base.Awake();
+		SuscribeSceneLoading();
+		State = initialState;
+		
+	}
+
+	private void Start() => LoadSettings(); // SettingsManager Has to Awake before loading
+
+	private void Update()
+	{
+		if (Input.GetKeyUp(KeyCode.Escape)) TogglePause();
+		if (Input.GetKeyUp(KeyCode.F12)) ToggleDebugMode();
+	}
+	
+	private void OnStateChange(GameState newState)
+	{
+		switch (newState)
+		{
+			case GameState.MainMenu:
+				// TODO
+				break;
+			case GameState.GameOver:
+				// TODO
+				break;
+			case GameState.Playing:
+				// TODO
+				break;
+			case GameState.Paused:
+				// TODO
+				break;
+			default:
+				throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+		}
+
+		onGameStateChanged?.Invoke();
+	}
+
+
+	#region SETTINGS
+	
+	private void LoadSettings()
+	{
+		_debugMode = SettingsManager.Instance.GetSetting<bool>("debugMode");
+	}
+
+	#endregion
+	
+	
+	#region PAUSE
+
+	public void Pause() => State = GameState.Paused;
+	public void Resume() => State = GameState.Playing;
+	public void TogglePause() => State = State == GameState.Paused ? GameState.Playing : GameState.Paused;
+
+	#endregion
+	
+
+	#region SCENE MANAGEMENT
+
+	private void SuscribeSceneLoading()
+	{
+		SceneManager.sceneLoaded += (scene, mode) =>
+		{
+			switch (scene.name)
+			{
+				case "MainMenu":
+					State = GameState.MainMenu;
+					break;
+				case "Game":
+					State = GameState.Playing;
+					break;
+				case "GameOver":
+					State = GameState.GameOver;
+					break;
+			}
+		};
+	}
+
+	public static void LoadScene(string sceneName) => SceneManager.LoadScene(sceneName);
+	public static void ReloadScene() => LoadScene(SceneManager.GetActiveScene().name);
+	public static void QuitGame()
+	{
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
+
+	#endregion
+
+
+	#region PLAYER DATA
+
+	private int _winCount;
+	private int _winStreak;
+	private bool _win;
+	
+	public bool Win => _win;
+	public int WinCount => _winCount;
+	public int WinStreak => _winStreak;
+
+	public void RegisterWin()
+	{
+		_win = true;
+		_winCount++;
+		_winStreak++;
+	}
+	
+	public void RegisterLose()
+	{
+		_win = false;
+		_winStreak = 0;
+	}
+
+	#endregion
+
+
+	#region DEBUG
+
+	private bool _debugMode = false;
+
+	public bool DebugMode
+	{
+		get => _debugMode;
+		set
+		{
+			_debugMode = value;
+			onToggleDebugMode?.Invoke(_debugMode);
+		}
+	}
+	
+	public Action<bool> onToggleDebugMode;
+
+	public bool ToggleDebugMode() => DebugMode = !DebugMode;
+
+	#endregion
+}
